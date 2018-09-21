688016bf2ad402a4804ad804aab4bf5f6bfde7ac
Shantanu Tripathi <shantanutripathi@ip-192-168-1-122.ec2.internal>
2018-09-21 12:49:23 +0400

    first commit

---
+++ devscore/src/main/java/A.java
@@ -0,0 +1,18 @@
+public class A {
+    private int val;
+    public void setVal(int val){
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public int processor(){
+        return val * 10;
+    }
+
+    public int processor2(){
+        return val * 20;
+    }
+}
